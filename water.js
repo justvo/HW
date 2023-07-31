2d98d2f26de4cca782a973f5af3c5dd535eef89e
justvo <129364256+justvo@users.noreply.github.com>
2023-07-31 15:17:47 +0300

    test2

--- water.js
+++ water.js
@@ -1,25 +1,40 @@
 function countWater(mountains){
-    var water;
-    for (let i = 0; i<mountains.length; i++){
-        for (let j = 0;j<mountains.length; j++){
-            if((mountains[i] <= mountains[j])){
-                if((j - i) >= 2){
-                    for(let k = i; k < j; k++  ){
-                        water += mountains[i] - mountains[k];
-                    }
-                }
-                if((j - i) <= -2){
-                    for(let k = j; k > i; k--  ){
-                        water += mountains[j] - mountains[k];
-                    }
-                }
+    let highMou = Math.max.apply(null, mountains);
+    let water = 0;
+    for (let i = 1; i<=highMou; i++){
+        console.log("i "+ i);
+        let flagState = 0;
+        let flagFin = 0;
+        let count =0;
+        for (let j = 0; j<mountains.length; j++){
+            if (mountains[j] < i ){
+                if(!flagState){
+                count =0;
+             }
                 
+                flagState = 1;
+                flagFin = 1;
+            
+            }else {
+                flagState = 0;
+                if (flagFin){
+                    flagFin = 0;
+                    water += count;
+                }
             }
+            if(flagState){
+                count +=1;
+            }   
+            console.log("count1 ="+ count);
         }
+        console.log("count ="+ count);
+        console.log("water ="+ water);
+
     }
     return water;
 }
-let mou = [2,1,5,0,3,4,7,2,3,1,0];
+
+let mou =  [2, 2, 1, 2, 2, 3, 0, 1, 2];
 result = countWater(mou);
 console.log(result) 
 
